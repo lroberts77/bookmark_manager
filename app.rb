3dc2c9d36f9b5ff782e4d8c4447fd493cef3e701
liam roberts <liamroberts77@googlemail.comgit config --global user.email liamroberts77@googlemail.com>
2020-03-11 17:23:31 +0000

    continuing step 10

--- app.rb
+++ app.rb
@@ -8,9 +8,20 @@ class BookmarkManager < Sinatra::Base
     end
 
     get '/bookmarks' do
-       p "hello" 
-       p @bookmarks = Bookmark.all
-       p erb :'bookmarks/index'
+       @bookmarks = Bookmark.all
+       erb :'bookmarks/index'
     end
+
+    post '/bookmarks' do
+        url = params['url']
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+        connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+        redirect '/bookmarks'
+    end
+    
+    get '/bookmarks/new' do
+        erb :"bookmarks/new"
+    end
+
     run! if app_file == $0
 end
